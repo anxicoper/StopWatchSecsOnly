8343b566476b23910507b70eaa2db49f7931843a
genericWorker <generic.worker@gmail.com>
2023-01-16 09:20:04 -0500

    getinit

--- stopwatch.js
+++ stopwatch.js
@@ -1,48 +1,54 @@
-window.onload = function () {
+window.onload =
+  function(){
+  //---vars to access html elements ---//
+  let seconds = 00;
+  let tens = 00;
+  let minutes = 00; 
   
-  var seconds = 00; 
-  var tens = 00; 
-  var appendTens = document.getElementById("tens")
-  var appendSeconds = document.getElementById("seconds")
-  var buttonStart = document.getElementById('button-start');
-  var buttonStop = document.getElementById('button-stop');
-  var buttonReset = document.getElementById('button-reset');
-  var Interval ;
+  let appendTens = document.getElementById("tens");
+  let appendSeconds = 
+      document.getElementById("seconds");
+ let appendMinutes = 
+      document.getElementById("minutes");
+  
+  let buttonStart = document.getElementById("button-start");
+  let buttonStop = document.getElementById("button-stop");
+  let buttonReset = document.getElementById("button-reset");
 
-  buttonStart.onclick = function() {
-    
+  let Interval;
+  
+  //---functions to start, stop, reset ---//
+  buttonStart.onclick = function(){
     clearInterval(Interval);
-     Interval = setInterval(startTimer, 10);
+    Interval = setInterval(startTimer,10);
   }
-  
-    buttonStop.onclick = function() {
-       clearInterval(Interval);
-  }
-  
 
-  buttonReset.onclick = function() {
-     clearInterval(Interval);
-    tens = "00";
-  	seconds = "00";
+  buttonStop.onclick = function(){
+    clearInterval(Interval);
+  }
+
+  buttonReset.onclick = function(){
+    clearInterval(Interval);
+    tens="00";
+    seconds = "00";
+    minutes = "00"; 
+    appendMinutes.innerHTML = minutes;
     appendTens.innerHTML = tens;
-  	appendSeconds.innerHTML = seconds;
+    appendSeconds.innerHTML = seconds;
   }
   
-   
-  
-  function startTimer () {
-    tens++; 
+  function startTimer(){
+    tens++;
     
     if(tens <= 9){
       appendTens.innerHTML = "0" + tens;
     }
     
-    if (tens > 9){
+    if(tens > 9){
       appendTens.innerHTML = tens;
-      
-    } 
+    }
     
-    if (tens > 99) {
+    if(tens> 99){
       console.log("seconds");
       seconds++;
       appendSeconds.innerHTML = "0" + seconds;
@@ -50,11 +56,15 @@ window.onload = function () {
       appendTens.innerHTML = "0" + 0;
     }
     
-    if (seconds > 9){
+    if(seconds > 9){
       appendSeconds.innerHTML = seconds;
     }
-  
+    if (seconds > 59) {
+        minutes++;
+        seconds = 0; 
+        appendSeconds.innerHTML = "0" + 0;
+        appendMinutes.innerHTML =  "0" +  minutes;
+    }
   }
   
-
-}
+};
